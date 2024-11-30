0965de2541abed43c5b7c8e1c9fe1b6d25a1368b
Lady-Margaret (Marge) Hagan <144915421+ladymargehagan@users.noreply.github.com>
2024-11-30 18:43:41 +0000

    Delete lostintranslation directory

--- lostintranslation/ajax/update-word-index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-$data = json_decode(file_get_contents('php://input'), true);
-$_SESSION['current_word_index'] = $data['index'];
-
-echo json_encode(['success' => true]); 
